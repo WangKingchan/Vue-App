60841aa99f411341c57254e7b4a4ea570926e0e5
kingchan <kingchan0858@qq.com>
2019-05-23 20:15:06 +0800

    info组件添加删除按钮并实现删除功能

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -2,12 +2,13 @@
     <div>
         <!-- v-show="index == current"如果当前这个item的index==store的current那就显示该条详情-->
         <div v-for="(item,index) in pageLists"
-             v-show="index == current"
+             v-show="index === current"
              :key="index">
-            <span>{{item.title}}</span>
-            <p>{{item.content}}</p>
+            <p class="newTitle">{{item.title}}</p>
+            <p class="newInfo">{{item.content}}</p>
         </div>
-        <div class="returnList" @click="returnList()">返 &nbsp; 回</div>
+        <button type="button" class="returnList" @click="returnList()">返 &nbsp; 回</button>
+        <button type="button" class="returnList deleteNews" @click="deleteNews()">删 &nbsp; 除</button>
     </div>
 </template>
 
@@ -18,18 +19,29 @@
         name: "Info",
         data () {
             return {
-
-                current: store.state.current,//获取store的current
+                // 当前新闻的索引
+                current: store.state.current//获取store的current
             }
         },
         computed: {
             pageLists () {
+                // 新闻列表条数
                 return store.state.lists
             }
         },
         methods:{
+            // 返回新闻列表
             returnList () {
-                this.$router.push('/home/list')
+                // 路由跳转实现返回列表
+                this.$router.push('/home/list');
+            },
+            // 删除当前这条新闻并返回列表
+            deleteNews(){
+                // 调用数组方法splice()删除当前这个元素
+                // 语法：arrayObject.splice(index,howmany)
+                store.state.lists.splice(store.state.current,1);
+                // 路由跳转实现返回列表
+                this.$router.push('/home/list');
             }
         }
     }
@@ -37,20 +49,31 @@
 
 <style scoped lang="scss">
     .returnList{
-        width: 80%;
+        width: 90%;
         height: 40px;
         line-height: 40px;
         background: #42b983;
-        margin: 0 auto;
+        border: none;
+        outline: none;
         color: #fff;
+        margin: 15px auto;
         font-size: 16px;
         border-radius: 5px;
     }
-    span{
+    .deleteNews{
+        background: #35495e;
+    }
+    .newTitle{
         border-bottom: 1px solid rgba(147, 147, 147, 0.4);
         margin: 10px 0;
         padding: 8px 0;
         font-weight: 600;
-        display: block;
+        text-align: left;
+        text-align: justify;
+    }
+    .newInfo{
+        text-align: left;
+        text-align: justify;
+        text-indent: 2em;
     }
 </style>
